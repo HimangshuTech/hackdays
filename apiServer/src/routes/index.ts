426c54cbcf5a99d6cef42deff73d6b651cdd47ee
Himangshu Deka <dekahimangshu702@gmail.com>
2026-04-25 19:31:12 +0530

    Create Signup  Features

--- apiServer/src/routes/index.ts
+++ apiServer/src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import authController from '../auth/auth.controller';
+import authRouter from '../auth/auth.route';
 const router = Router();
 
+router.use("/auth",authRouter)
 
 export default router;
