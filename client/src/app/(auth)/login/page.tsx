e281398bb765151e3bfa465cccedf065916765ca
Dhanjyoti <dhanjyotidas00@gmail.com>
2026-04-25 19:37:07 +0530

    working on frontend

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,14 @@
+import LoginBox from "@/components/loginBox"
+
+export default function Login() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="">
+        <LoginBox></LoginBox>
+      </div>
+
+
+    </div>
+  )
+
+}
