5a8c3c8fe8d6f4677bcf873c75867a1688d0e4d3
Himangshu Deka <dekahimangshu702@gmail.com>
2026-04-29 19:51:10 +0530

    Comments Functionalities Design

--- apiServer/src/routes/index.ts
+++ apiServer/src/routes/index.ts
@@ -3,12 +3,12 @@ import authRouter from '../module/auth/auth.route';
 import postRouter from '../module/post/post.router'
 import searchRouter from '../module/search/search.router'
 import recRouter from '../module/recommendation/rec.router'
-
+import commentRoutes from '../module/comments/comments.route'
 const router = Router();
 
 router.use("/auth", authRouter)
 router.use("/post", postRouter)
 router.use("/search", searchRouter)
 router.use("/recommend", recRouter)
-
+router.use("/comments", commentRoutes);
 export default router;
